c2f81dd046ea95c3c5c1529c5fe99c5eb67036d7
rafal <rafal_markowski@onet.pl>
2019-01-05 14:00:05 +0100

    Wygląd zakładek

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
+import * as $ from 'jquery';
 import '../styles/main.scss';
+
+$(() => {
+    const classes = {
+        js: 'tabs--js',
+        noJs: 'tabs--no-js'
+    };
+    const $tabs = $('.tabs');
+    
+    $tabs.addClass(classes.js).removeClass(classes.noJs);
+    
+    $('#js').on('click', e => {
+        e.preventDefault();
+        $tabs.addClass(classes.js).removeClass(classes.noJs);
+    });
+
+    $('#no-js').on('click', e => {
+        e.preventDefault();
+        $tabs.addClass(classes.noJs).removeClass(classes.js);
+    })
+});
